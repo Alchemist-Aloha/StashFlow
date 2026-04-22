c48f2c40f4671fca048ac92733701d35702481ad
Likun Cai <kentclk@outlook.com>
2026-04-22 13:50:57 -0400

    fix: add missing import for services in native video controls

--- lib/features/scenes/presentation/widgets/native_video_controls.dart
+++ lib/features/scenes/presentation/widgets/native_video_controls.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../../../../core/utils/l10n_extensions.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:video_player/video_player.dart';
